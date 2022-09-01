d85e71d5cce73dd08b790daa7ea9f75cc98f863e
Leonardo Albornoz <albornozleo95@gmail.com>
2022-09-01 15:29:58 -0400

    Create entity cliente

--- src/main/java/com/banco/app/entities/Cliente.java
+++ src/main/java/com/banco/app/entities/Cliente.java
@@ -1,5 +1,10 @@
 package com.banco.app.entities;
 
+import javax.persistence.Table;
+import javax.persistence.Entity;
+
+@Entity
+@Table(name = "Clientes")
 public class Cliente extends Persona {
 
   public Cliente(String nombre, String genero, int edad, int identificacion, String direccion, String telefono) {
