bc7a955e2a0ab04e629dfe6ae4fcad543f4be588
Leonardo Albornoz <albornozleo95@gmail.com>
2022-09-01 12:13:46 -0400

    First commit

---
+++ src/main/java/com/banco/demo/ApplicationProgrammingInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.banco.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationProgrammingInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationProgrammingInterfaceApplication.class, args);
+	}
+
+}
