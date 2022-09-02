56cb4a2877e59392356befe9f0517af677b5ca49
Leonardo Albornoz <albornozleo95@gmail.com>
2022-09-01 20:39:57 -0400

    Add service to cliente root

--- src/main/java/com/banco/app/controllers/ClienteController.java
+++ src/main/java/com/banco/app/controllers/ClienteController.java
@@ -1,19 +1,26 @@
 package com.banco.app.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banco.app.entities.Cliente;
+import com.banco.app.services.IClienteService;
 
 /**
  * ClienteController
  */
-@Controller
+@RestController
 public class ClienteController {
 
+  @Autowired
+  private IClienteService service;
+
+  @GetMapping("/clientes")
   public List<Cliente> getClientes() {
-    List<Cliente> clientes = new ArrayList<>();
-    return clientes;
+    return service.getClientes();
   }
 
 }
