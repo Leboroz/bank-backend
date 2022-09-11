9d625a0e41208ab97cb88b8c7cbdf5cf947820df
Leonardo Albornoz <albornozleo95@gmail.com>
2022-09-10 20:35:14 -0400

    Refactor Client service

---
+++ src/main/java/com/banco/app/services/IClientService.java
@@ -0,0 +1,19 @@
+package com.banco.app.services;
+
+import java.util.List;
+import com.banco.app.entities.Client;
+
+/**
+ * IClientService
+ */
+public interface IClientService {
+  List<Client> getClientes();
+
+  List<Client> getClientsWithAccounts();
+
+  Client getClientById(Long id);
+
+  void deleteClientById(Long id);
+
+  void saveClient(Client client);
+}
